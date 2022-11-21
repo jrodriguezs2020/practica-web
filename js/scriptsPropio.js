cb2b346173b1393fb3677c7a1e358788ead36788
javii <javiisalaas97@gmail.com>
2022-11-21 20:47:11 +0100

    añadir eventos vol.1

--- js/scriptsPropio.js
+++ js/scriptsPropio.js
@@ -32,7 +32,7 @@ function loadPage() {
     </div>
     <!-- Botón añadir evento-->
     <div class="btnAdd">
-        <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" href="#addEvent">Añadir
+        <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" onclick=addTicket();>Añadir
             evento</button>
     </div>`;
 }
@@ -127,6 +127,7 @@ function goback() {
     let content1 = document.getElementById('portfolio');
     let content2 = document.getElementById('portfolio2');
     let content3 = document.getElementById('buyPass');
+    let content4 = document.getElementById("addEvent2");
     if (content1.style.display == 'block') {
         content1.style.display = 'none';
         content2.style.display = 'block';
@@ -138,6 +139,10 @@ function goback() {
     if (content3.style.display == 'block') {
         content3.style.display = 'none';
         content2.style.display = 'block';
+    }
+    if(content4.style.display == 'block'){
+        content4.style.display = 'none';
+        content1.style.display = 'block';
     }
 
     /*
@@ -181,20 +186,79 @@ function showTicket(i) {
 
 function addPass() {
     let element = document.getElementById('subelementos');
-    element.innerHTML = `<div class="form-group">
-                            <input class="form-control" id="pass" type="text"
+    element.innerHTML = `<br />
+                        <div class="form-group">
+                            <input class="form-control" id="titlePass" type="text"
                                 placeholder="Título de la entrada *"
                                 data-sb-validations="required" />
                             <div class="invalid-feedback" data-sb-feedback="pass:required">
                                 Es necesario un título</div>
                         </div>
                         <div class="form-group">
-                            <input class="form-control" id="price" type="int"
+                            <input class="form-control" id="pricePass" type="int"
                                 placeholder="Precio de la entrada *"
                                 data-sb-validations="required" />
                             <div class="invalid-feedback" data-sb-feedback="price:required">
                                 Es necesario un precio</div>
-                        </div>`
+                        </div>`;
+    //return 1;
+}
+
+function addTicket() {
+    let content1 = document.getElementById('portfolio');
+    let content2 = document.getElementById('addEvent2');
+    content1.style.display = 'none';
+    content2.style.display = 'block';
+    showForm();
+    let boton = document.getElementById("botones")
+    boton.innerHTML = `
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" onclick="goback();">Volver atrás</button>
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" onclick="saveEvent();">Crear evento</button>
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" id="addEvent" onclick="addPass();">Añadir entrada</button>`;
+}
+
+function showForm() {
+    let content = document.getElementById("addEvent2");
+    content.innerHTML =
+        `<div class="container"> <!--meter en metodo y llamar en showInfo-->
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="modal-body">
+                        <div class="text-center">
+                            <h2 class="section-heading text-uppercase">Nuevo evento</h2>
+                            <section class="page-section" id="contact" style="background-color:#ffffff">
+                                <!--page-section define el espacio entre el titulo y las cajas del form-->
+                                <div class="container">
+                                    <form id="contactForm">
+                                        <div class="row align-items-stretch mb-5">
+                                            <!-- mb-5 define el espacio entre los botones y principio/final-->
+                                                <div class="form-group">
+                                                    <!-- Title input-->
+                                                    <input class="form-control" id="titleEvent" type="text"
+                                                        placeholder="Título del evento *"
+                                                        data-sb-validations="required" />
+                                                </div>
+                                                <div class="form-group form-group-textarea mb-md-0">
+                                                    <!-- Description input-->
+                                                    <textarea class="form-control" id="descriptionEvent"
+                                                        placeholder="Descripción *"
+                                                        data-sb-validations="required"></textarea>
+                                                </div>
+                                            <div id="subelementos">
+                                                <!--Campos del subelemento-->
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="btnAdd" id="botones">
+            <!-- botones volver, añadir entrada y crear -->
+        </div>`
 }
 
 function deleteEvent(i) {
@@ -208,6 +272,38 @@ function deleteEvent(i) {
     }
     else {
         showInfo(i);
+    }
+}
+
+function saveEvent() {
+    let message = "¿Está seguro que quiere crear este evento?"
+    let option = alerta(message);
+    if (option == 1) {
+        //recojo información
+        let titulo = document.getElementById("titleEvent").value;
+        let descripcion = document.getElementById("descriptionEvent").value;
+        console.log(titulo);
+        if (titulo == "") {
+            titulo = "Sin título";
+        }
+        if (descripcion == "") {
+            descripcion = "Sin descripción";
+        }
+        /*
+        let tituloEntrada = document.getElementById("titlePass").value;
+        let precio = document.getElementById("pricePass").value;
+        if (tituloEntrada != null) {
+                    let entradaEvent = {titleEntrada: tituloEntrada, price: precio }
+                entradas.push(entradaEvent);
+        }*/
+        let evento = { titleEvento: titulo, descriptionEvento: descripcion, entradas };
+        eventos.push(evento);
+        loadPage();
+        showEvent();
+        goback();
+    }
+    else {
+        goback();
     }
 }
 
