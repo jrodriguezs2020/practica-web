2ae6a2dd774b617331f6b083abeafdf93bfdf0cd
javii <javiisalaas97@gmail.com>
2022-11-23 12:27:04 +0100

    interfaz modificar evento

--- js/scriptsPropio.js
+++ js/scriptsPropio.js
@@ -140,7 +140,7 @@ function goback() {
         content3.style.display = 'none';
         content2.style.display = 'block';
     }
-    if(content4.style.display == 'block'){
+    if (content4.style.display == 'block') {
         content4.style.display = 'none';
         content1.style.display = 'block';
     }
@@ -165,12 +165,34 @@ function showInfo(i) {
     showTicketsEvent();
     let buttons = document.getElementById("buttons");
     buttons.innerHTML = `<button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" onclick="deleteEvent(${i});">Borrar evento</button>
-    <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" href="#addPass">Añadir
-        entrada</button>
     <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal"
-        href="#addEvent">Modificar</button>
+        onclick="modifyTicket()">Modificar</button>
     <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" onclick="goback();">Volver
         atrás</button>`
+}
+
+function modifyTicket() {
+    let content1 = document.getElementById('portfolio2');
+    let content2 = document.getElementById('addEvent2');
+    content1.style.display = 'none';
+    content2.style.display = 'block';
+    showForm();
+    let titleInfoMod = document.getElementById("titleInfo");
+    titleInfoMod.innerHTML = "Modificar evento";
+
+    let element = document.getElementById('subelementos');
+    for (let i = 0; i < entradas.length; i++) {
+        addPass();
+        element.innerHTML += `<button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal">Borrar entrada &#9757;</button>
+                               <br />`;
+        estado = false;
+    }
+
+    let boton = document.getElementById("botones")
+    boton.innerHTML = `
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" onclick="goback();">Volver atrás</button>
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal">Guardar</button>
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" id="addEvent" onclick="addPass();">Añadir entrada</button>`
 }
 
 function showTicket(i) {
@@ -183,11 +205,12 @@ function showTicket(i) {
     let description = document.getElementById("priceTicket");
     description.innerHTML = `Precio: ` + entradas[i].price + `€`;
 }
+
 let estado = false;
 function addPass() {
-    estado = true;
-    let element = document.getElementById('subelementos');
-    element.innerHTML = `<br />
+    if (estado == false) {
+        let element = document.getElementById('subelementos');
+        element.innerHTML += `<br />
                         <div class="form-group">
                             <input class="form-control" id="titlePass" type="text"
                                 placeholder="Título de la entrada *"
@@ -202,7 +225,8 @@ function addPass() {
                             <div class="invalid-feedback" data-sb-feedback="price:required">
                                 Es necesario un precio</div>
                         </div>`;
-    //return 1;
+    }
+    estado = true;
 }
 
 function addTicket() {
@@ -211,6 +235,8 @@ function addTicket() {
     content1.style.display = 'none';
     content2.style.display = 'block';
     showForm();
+    let titleInformative = document.getElementById("titleInfo");
+    titleInformative.innerHTML = "Nuevo evento";
     let boton = document.getElementById("botones")
     boton.innerHTML = `
     <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" onclick="goback();">Volver atrás</button>
@@ -226,7 +252,7 @@ function showForm() {
                 <div class="col-lg-8">
                     <div class="modal-body">
                         <div class="text-center">
-                            <h2 class="section-heading text-uppercase">Nuevo evento</h2>
+                            <h2 class="section-heading text-uppercase" id="titleInfo"></h2>
                             <section class="page-section" id="contact" style="background-color:#ffffff">
                                 <!--page-section define el espacio entre el titulo y las cajas del form-->
                                 <div class="container">
@@ -286,7 +312,7 @@ function saveEvent() {
         /*sin entrada en una section y con en otra */
         let titulo = document.getElementById("titleEvent").value;
         let descripcion = document.getElementById("descriptionEvent").value;
-        
+
         if (titulo == "") {
             titulo = "Sin título";
         }
@@ -297,12 +323,12 @@ function saveEvent() {
         let evento = { titleEvento: titulo, descriptionEvento: descripcion, entradas };
         eventos.push(evento);
 
-        if (estado == true){
+        if (estado == true) {
             let tituloEntrada = document.getElementById("titlePass").value;
             let precio = document.getElementById("pricePass").value;
-            let entradaEvent = {titleEntrada: tituloEntrada, price: precio }
+            let entradaEvent = { titleEntrada: tituloEntrada, price: precio }
             entradas.push(entradaEvent);
-            
+
 
         }
         loadPage();
