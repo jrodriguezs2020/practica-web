41fbaa1de293ab94584c5f18529848d662049a27
javii <javiisalaas97@gmail.com>
2022-11-27 12:52:25 +0100

    cambio estructura principal, comentarios y valores

--- js/scriptsPropio.js
+++ js/scriptsPropio.js
@@ -1,19 +1,50 @@
-/* ESTRUCTURA DE LOS ELEMENTOS Y SUBELEMENTOS*/
+/* ESTRUCTURA DE LOS ELEMENTOS Y SUBELEMENTOS */
+class Evento {
+    constructor(titleEvento, descriptionEvento, entradasEvento) {
+        this.titleEvento = titleEvento;
+        this.descriptionEvento = descriptionEvento;
+        this.entradasEvento = entradasEvento;
+    }
+}
+
+class Entrada {
+    constructor(titleEntrada, price) {
+        this.titleEntrada = titleEntrada;
+        this.price = price;
+    }
+}
+/*---------------------------------------------------------------*/
+/* Datos de ejemplo */
+let tickets = [];
+let t1 = new Entrada("Abono General", 37);
+let t2 = new Entrada("Abono General + Promo Bebida", 52);
+tickets.push(t1);
+tickets.push(t2);
+
+let tickets2 = [];
+let t12 = new Entrada("Abono General", 55);
+let t22 = new Entrada("Abono General + Acampada", 80);
+let t32 = new Entrada("Abono VIP", 100);
+let t42 = new Entrada("Abono VIP + Acampada", 120);
+tickets2.push(t12);
+tickets2.push(t22);
+tickets2.push(t32);
+tickets2.push(t42);
+
+let tickets3 = [];
+let t13 = new Entrada("Abono General", 45);
+let t23 = new Entrada("Abono Total Immersion", 110);
+tickets3.push(t13);
+tickets3.push(t23);
+
 let eventos = [];
-let entradas = [];
-let entrada1 = { titleEntrada: "VIP", price: 80 }
-let entrada2 = { titleEntrada: "General", price: 50 }
-
-entradas.push(entrada1);
-entradas.push(entrada2);
-
-let evento1 = { titleEvento: "Madrid Salvaje", descriptionEvento: "El mejor del país", entradas };
-let evento2 = { titleEvento: "Weekend Beach", descriptionEvento: "El peor del país", entradas };
-
-eventos.push(evento1);
-eventos.push(evento2);
-/*----------------------------------------------------------------------------------------------------*/
-
+let e1 = new Evento("Madrid Salvaje", "Madrid Salvaje nació con el fin de derribar lo políticamente correcto, sin prejuicios. Nació como un espacio creado por y para los amantes de la música urbana, como un punto de encuentro entre los artistas más reconocidos y los nombres más emergentes de la escena underground del país. En muy poco tiempo nos hicimos grandes, conquistamos Madrid e hicimos que se nos escuchase. Madrid Salvaje es el punto de encuentro de la música urbana durante dos días de festival, con más de 35 de los artistas de rap y trap más importantes del país actuando en 3 escenarios. Todo ello acompañado de exhibiciones de skate, graffiti, foodtrucks y mucho más. Si eres uno de los nuestros, este es tu sitio.", tickets);
+let e2 = new Evento("Weekend Beach Festival", "Uno de los grandes escenarios musicales en el calendario de verano. Un regreso que cumplirá las expectativas de público y organización. Con más de 80 artistas que pasarán por el recinto del festival en esta séptima edición destacando artistas internacionales como Bastille o Nicky Jam, que encabezarán esta edición del festival junto a muchos otros. Conciertos espectaculares en un entorno único con todos los accesos y comodidades harán del festival una de las citas musicales imprescindibles del verano en nuestro país.", tickets2);
+let e3 = new Evento("The Juergas Rock Festival", "¡BOOM! Madrid está más que preparada para recibiros los días 02, 03, 04 y 05 de agosto del año que viene, como lo oyes, ¡4 pedazo de días de juerga! Y no va a ser una juerga cualquiera, porque además vamos a celebrar nuestro  X ANIVERSARIO. Se cumplen diez añazos de la primera vez que se celebró la primera edición del que ahora es uno de los mejores festivales de rock en nuestro país, así que, prepararos, que la vamos a liar bien fuerte. ¡APUNTATE YA!", tickets3);
+eventos.push(e1);
+eventos.push(e2);
+eventos.push(e3);
+/*---------------------------------------------------------------*/
 /*----- Métodos de inicio -----*/
 loadPage();
 showEvent();
@@ -37,50 +68,68 @@ function loadPage() {
     </div>`;
 }
 
+/*----- Muestra los eventos en la PP -----*/
 function showEvent() {
     let events = document.getElementById('showEvents');
-    //control de eventos (si no hay eventos)
-    for (let i = 0; i < eventos.length; i++) {
-        events.innerHTML +=
-            `<div class="col-lg-4 col-sm-6 mb-4">
-        <div class="portfolio-item">
-            <a class="portfolio-link" onclick="showInfo(${i});">
-                <div class="portfolio-hover">
-                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                </div>
-                <img class="img-fluid" src="assets/img/portfolio/1.jpg" alt="..." />
-            </a>
-            <div class="portfolio-caption">
-            <div class="portfolio-caption-heading">`+ eventos[i].titleEvento + `</div>
-            </div>
-        </div>
-    </div>`
-    };
-}
-
-/* Muestra las entradas de cada evento en su sección correspondiente */
-/* necesitará cambios */
-function showTicketsEvent() {
-    let pass = document.getElementById('showPass');
-    //control de entradas (si no hay entradas)
-    for (let i = 0; i < entradas.length; i++) {
-        pass.innerHTML +=
-            `<div class="col-lg-4 col-sm-6 mb-4">
-        <div class="portfolio-item">
-            <a class="portfolio-link" onclick="showTicket(${i})"> 
-                <div class="portfolio-hover">
-                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                </div>
-                <img class="img-fluid" src="assets/img/portfolio/2.jpg" alt="..." />
-            </a>
-            <div class="portfolio-caption">
-                <div class="portfolio-caption-heading">` + entradas[i].titleEntrada + `</div>
-            </div>
-        </div>
-    </div>`
+    if (eventos.length == 0) {
+        events.innerHTML = `<div class="text-center">
+            <h3 class="section-heading text-uppercase">No hay eventos disponibles</h3>
+            <br></br>
+        </div>`;
+    }
+    else {
+        for (let i = 0; i < eventos.length; i++) {
+            events.innerHTML +=
+                `<div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="portfolio-item">
+                        <a class="portfolio-link" onclick="showInfo(${i});">
+                            <div class="portfolio-hover">
+                                <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img class="img-fluid" src="assets/img/portfolio/1.jpg" alt="..." />
+                        </a>
+                        <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">`+ eventos[i].titleEvento + `</div>
+                        </div>
+                    </div>
+                </div>`
+        };
     }
 }
 
+/*----- Muestra las entradas de cada evento en su sección correspondiente -----*/
+let num;
+function showTicketsEvent(i) {
+    num = i;
+    let entradas = eventos[i].entradasEvento;
+    let pass = document.getElementById('showPass');
+    if (entradas.length == 0) {
+        pass.innerHTML = `<div class="text-center">
+            <h3 class="section-heading text-uppercase">No hay entradas disponibles</h3>
+            <br></br>
+        </div>`;
+    }
+    else {
+        for (let j = 0; j < entradas.length; j++) {
+            pass.innerHTML +=
+                `<div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="portfolio-item">
+                        <a class="portfolio-link" onclick="showTicket(${j})"> 
+                            <div class="portfolio-hover">
+                                <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img class="img-fluid" src="assets/img/portfolio/2.jpg" alt="..." />
+                        </a>
+                        <div class="portfolio-caption">
+                            <div class="portfolio-caption-heading">` + entradas[j].titleEntrada + `</div>
+                        </div>
+                    </div>
+                </div>`
+        }
+    }
+}
+
+/*----- Muestra la interfaz de info del evento -----*/
 function showportfolio2() {
     let portf = document.getElementById("portfolio2");
     portf.innerHTML = `<div class="container"> <!--meter en metodo y llamar en showInfo-->
@@ -121,7 +170,6 @@ function showportfolio2() {
         </div>`
 }
 
-
 /* none = ocultar, block = mostrar */
 function goback() {
     let content1 = document.getElementById('portfolio');
@@ -144,14 +192,9 @@ function goback() {
         content4.style.display = 'none';
         content1.style.display = 'block';
     }
-
-    /*
-        let content1 = document.getElementById('portfolio');
-        let content2 = document.getElementById('portfolio2');
-        content1.style.display = 'block';
-        content2.style.display = 'none';*/
 }
 
+/*----- Muestra la info del evento -----*/
 function showInfo(i) {
     let content1 = document.getElementById('portfolio');
     let content2 = document.getElementById('portfolio2');
@@ -162,7 +205,7 @@ function showInfo(i) {
     titulo.innerHTML = eventos[i].titleEvento;
     let description = document.getElementById("descriptionInfoEvent");
     description.innerHTML = eventos[i].descriptionEvento;
-    showTicketsEvent();
+    showTicketsEvent(i);
     let buttons = document.getElementById("buttons");
     buttons.innerHTML = `<button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" onclick="deleteEvent(${i});">Borrar evento</button>
     <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal"
@@ -171,12 +214,15 @@ function showInfo(i) {
         atrás</button>`
 }
 
+/*----- Botón modificar, muestra formulario -----*/
 function modifyTicket(i) {
+    num = i;
     let content1 = document.getElementById('portfolio2');
     let content2 = document.getElementById('addEvent2');
     content1.style.display = 'none';
     content2.style.display = 'block';
     let evento = eventos[i];
+    let entradas = evento.entradasEvento;
     showForm();
     changesValues(evento);
 
@@ -203,7 +249,9 @@ function modifyTicket(i) {
     <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" id="addEvent" onclick="addPass();">Añadir entrada</button>`
 }
 
-function deleteTicket(z,i) {
+/*----- Elimina una entrada -----*/
+function deleteTicket(z) {
+    let entradas = eventos[num].entradasEvento;
     let message = "¿Está seguro que quiere borrar esta entrada?"
     let option = alerta(message);
     if (option == 1) {
@@ -218,6 +266,7 @@ function deleteTicket(z,i) {
     }
 }
 
+/*----- Cambia valores en modificar -----*/
 function changesValues(evento) {
     let titleInfoMod = document.getElementById("titleInfo");
     titleInfoMod.innerHTML = "Modificar evento";
@@ -227,8 +276,9 @@ function changesValues(evento) {
     descripcion.value = evento.descriptionEvento;
 }
 
+/*----- Guarda info de modificar -----*/
 function saveInfo(i) {
-    let message = "¿Está seguro que quiere guardar la informacón modificada?"
+    let message = "¿Está seguro que quiere guardar la información modificada?"
     let option = alerta(message);
     if (option == 1) {
         let titulo = document.getElementById("titleEvent").value;
@@ -244,17 +294,19 @@ function saveInfo(i) {
         eventos[i].titleEvento = titulo;
         eventos[i].descriptionEvento = descripcion;
 
-        for(let z=0; z<entradas.length; z++){
-            let titleTicket = document.getElementById("titlePass" + z).value;
-            entradas[z].titleEntrada = titleTicket;
-            let priceTicket = document.getElementById("pricePass" + z).value;
-            entradas[z].price = priceTicket;
+        let entradas = eventos[i].entradasEvento;
+        if (entradas.length != 0) {
+            for (let x = 0; x < entradas.length; x++) {
+                let tituloTicket = document.getElementById("titlePass" + x).value;
+                entradas[x].titleEntrada = tituloTicket;
+                let precioTicket = document.getElementById("pricePass" + x).value;
+                entradas[x].price = precioTicket;
+            }
         }
-
         if (estado == true) {
             let tituloEntrada = document.getElementById("titlePass").value;
-            let precio = document.getElementById("pricePass").value;
-            let entradaEvent = { titleEntrada: tituloEntrada, price: precio }
+            let precioEntrada = document.getElementById("pricePass").value;
+            let entradaEvent = new Entrada(tituloEntrada, precioEntrada)
             entradas.push(entradaEvent);
         }
 
@@ -264,22 +316,25 @@ function saveInfo(i) {
         estado = false;
     }
     else {
-        goback();
+        modifyTicket(i);
     }
 }
 
-function showTicket(i) {
+/*----- Muestra interfaz e info de la entrada -----*/
+function showTicket(j) {
+    let entrada = eventos[num].entradasEvento[j];
     let content1 = document.getElementById("buyPass");
     let content2 = document.getElementById("portfolio2");
     content2.style.display = 'none';
     content1.style.display = 'block';
     let titulo = document.getElementById("titleTicket");
-    titulo.innerHTML = entradas[i].titleEntrada;
+    titulo.innerHTML = entrada.titleEntrada;
     let description = document.getElementById("priceTicket");
-    description.innerHTML = `Precio: ` + entradas[i].price + `€`;
+    description.innerHTML = `Precio: ` + entrada.price + `€`;
 }
 
 let estado = false;
+/*----- Añadir entrada -----*/
 function addPass() {
     if (estado == false) {
         let element = document.getElementById('subelementos');
@@ -298,6 +353,7 @@ function addPass() {
     estado = true;
 }
 
+/*----- Interfaz añadir evento -----*/
 function addTicket() {
     let content1 = document.getElementById('portfolio');
     let content2 = document.getElementById('addEvent2');
@@ -313,6 +369,7 @@ function addTicket() {
     <button class="btn btn-primary btn-xl text-uppercase" data-bs-toggle="modal" id="addEvent" onclick="addPass();">Añadir entrada</button>`;
 }
 
+/*----- Muestra formulario -----*/
 function showForm() {
     let content = document.getElementById("addEvent2");
     content.innerHTML =
@@ -357,6 +414,7 @@ function showForm() {
         </div>`
 }
 
+/*----- Borra evento -----*/
 function deleteEvent(i) {
     let message = "¿Está seguro que quiere borrar el evento?"
     let option = alerta(message);
@@ -371,6 +429,7 @@ function deleteEvent(i) {
     }
 }
 
+/*----- Guarda evento creado -----*/
 function saveEvent() {
     let message = "¿Está seguro que quiere crear este evento?"
     let option = alerta(message);
@@ -384,26 +443,31 @@ function saveEvent() {
         if (descripcion == "") {
             descripcion = "Sin descripción";
         }
-        console.log(titulo);
-        let evento = { titleEvento: titulo, descriptionEvento: descripcion, entradas };
+
+        let entradas = [];
+        let evento = new Evento(titulo, descripcion, entradas);
         eventos.push(evento);
 
         if (estado == true) {
             let tituloEntrada = document.getElementById("titlePass").value;
             let precio = document.getElementById("pricePass").value;
-            let entradaEvent = { titleEntrada: tituloEntrada, price: precio }
+            if (tituloEntrada == "") {
+                tituloEntrada = "Sin título";
+            }
+            if (precio == "") {
+                precio = 100; //precio por defecto
+            }
+            let entradaEvent = new Entrada(tituloEntrada, precio);
             entradas.push(entradaEvent);
-
-
         }
         loadPage();
         showEvent();
         goback();
         estado = false;
-        console.log(estado);
     }
     else {
         goback();
+        estado = false;
     }
 }
 
