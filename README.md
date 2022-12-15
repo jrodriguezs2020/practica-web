abb5d1a825255cb4398386e4375bb2d3cc9fd588
javii <javiisalaas97@gmail.com>
2022-12-15 17:14:53 +0100

    creacion de mustache

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Práctica fundamentos de la web
+[![license](https://img.shields.io/github/license/jrodriguezs2020/practica-web.svg)](https://github.com/jrodriguezs2020/practica-web/blob/main/License)
+
 
 Una web de venta de entradas
 
--- src/router.js
+++ src/router.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import * as boardService from './eventsService.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
 
-    res.render('index', {
-        name: "World"
+    res.render('index', { 
+        posts: boardService.getPosts() 
     });
+});
+
+router.get('/post/:id', (req, res) => {
+
+    let post = boardService.getPost(req.params.id);
+
+    res.render('show_event', { post });
 });
 
 export default router;
