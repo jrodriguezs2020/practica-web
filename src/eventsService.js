52e7620ad0338fe5a50d9b044004f60864f9dab0
javii <javiisalaas97@gmail.com>
2022-12-20 16:15:42 +0100

    nuevas pantallas y modificaciones

--- src/eventsService.js
+++ src/eventsService.js
@@ -1,50 +1,57 @@
-/*const posts = new Map();
-let nextId = 0;
-
-addPost({ user: "Pepe", title: "Vendo moto", text: "Barata, barata" });
-addPost({ user: "Juan", title: "Compro coche", text: "Pago bien" });
-
-export function addPost(post) {
-    let id = nextId++; //por qué si id = 0+1=1 y el primer post es id = 0
-    post.id = id.toString();
-    posts.set(post.id, post);
-}
-
-export function deletePost(id){
-    posts.delete(id);
-}
-
-export function getPosts(){
-    return [...posts.values()];
-}
-
-export function getPost(id){
-    return posts.get(id);
-}*/
-
 const events = new Map();
 const tickets1 = new Map();
 const tickets2 = new Map();
 
+class Evento {
+    constructor(titleEvent, descriptionEvent, ticketsEvent) {
+        this.titleEvent = titleEvent;
+        this.descriptionEvent = descriptionEvent;
+        this.ticketsEvent = ticketsEvent;
+    }
+}
+
+class Entrada {
+    constructor(titleTicket, price) {
+        this.titleTicket = titleTicket;
+        this.price = price;
+    }
+}
+
 let idd = 0;
 let idt = 0;
+//let tickets = new Map();
+//let events = new Map();
 
-export function loadSampleData(){
-addTicket({titleTicket: "Abono General + Acampada", price: 80});
-addTicket({titleTicket: "abc", price: 80});
+export function loadSampleData() {
+    /*addEvent({titleEvent: "Madrid", descriptionEvent: "Prueba", ticketsEvent: new Map()});
+    addEvent({titleEvent: "Weekend", descriptionEvent: "beach", ticketsEvent: new Map()});
 
-addEvent({titleEvent: "Madrid", descriptionEvent: "Prueba", ticketsEvent: tickets2});
-addEvent({titleEvent: "Weekend", descriptionEvent: "beach", ticketsEvent: tickets2});
+    addTicket({titleTicket: "Abono General + Acampada", price: 80});
+    addTicket({titleTicket: "abc", price: 80});
 
-addEvent({titleEvent: "paco", descriptionEvent: "Prueba", ticketsEvent: tickets2});
-addEvent({titleEvent: "manolo", descriptionEvent: "beach", ticketsEvent: tickets2});
+    addEvent({titleEvent: "paco", descriptionEvent: "Prueba", ticketsEvent: tickets2});
+    addEvent({titleEvent: "manolo", descriptionEvent: "beach", ticketsEvent: tickets2});
 
-addEvent({titleEvent: "holi", descriptionEvent: "Prueba", ticketsEvent: tickets2});
-addEvent({titleEvent: "adioli", descriptionEvent: "beach", ticketsEvent: tickets2});
+    addTicket({titleTicket: "Abono General + Acampada", price: 80});*/
+
+    let e1 = new Evento("Madrid Salvaje", "Si eres uno de los nuestros, este es tu sitio.", tickets1);
+    addEvent(e1);
+
+    let t1 = new Entrada("Abono General", 37);
+    addTicket(t1);
+    
 }
+
 loadSampleData();
 
 export function addEvent(event) {
+    event.id = idd.toString();
+    events.set(event.id, event);
+    idd++;
+}
+
+export function addEvent2(title, description, tickets) {
+    let event = new Event(title, description, tickets);
     event.id = idd.toString();
     events.set(event.id, event);
     idd++;
@@ -63,28 +70,36 @@ export function getEvents(from, to) {
     }
 }
 
-export function getEvent(id){
+export function getEvent(id) {
     return events.get(id);
 }
 
 export function addTicket(ticket) {
+    let event = events.get(0);
     ticket.id = idt.toString();
-    tickets2.set(ticket.id, ticket);
+
+    //event.ticketsEvent.set(ticket.id, ticket);
+    //console.log(event.ticketsEvent.size);
+    tickets1.set(ticket.id, ticket);
     idt++;
 }
-/*
+
 export function addTicket1(ticket) {
     ticket.id = idt.toString();
     tickets1.set(ticket.id, ticket);
     idt++;
 }
-*/
-export function getTickets(event){
+
+export function getTickets(event) {
     return [...event.ticketsEvent.values()];
 }
 
-export function getTicket(id){
-    return tickets2.get(id);
+export function getTicket(event, id) {
+    return event.ticketsEvent.get(id);
+}
+
+export function deleteEvent(id) {
+    return events.delete(id);
 }
 
 /* como tener solo un addticket para todos los eventos */
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as boardService from './eventsService.js';
 import { getEvents } from './eventsService.js';
+import yesno from 'yesno';
 
 const router = express.Router();
 /*
@@ -14,11 +15,12 @@ router.get('/', (req, res) => {
 
 router.get('/', (req, res) => {
 
-    const events = getEvents(0,3);
+    const events = getEvents(0, 3);
 
     res.render('index', {
         events: events
     });
+
 });
 
 router.get('/events', (req, res) => {
@@ -26,7 +28,7 @@ router.get('/events', (req, res) => {
     const from = parseInt(req.query.from);
     const to = parseInt(req.query.to);
 
-    const events = getEvents(from,to);
+    const events = getEvents(from, to);
 
     res.render('events', {
         events: events
@@ -38,15 +40,42 @@ router.get('/event/:id', (req, res) => {
     let event = boardService.getEvent(req.params.id);
     let tickets = boardService.getTickets(event);
 
-    res.render('show_event', { event , tickets});
+    res.render('show_event', { event, tickets });
 });
 
-router.get('/event/:id/showTicket/:id', (req, res) => {
+router.get('/event/:id/showTicket/:idt', (req, res) => {
 
-    let ticket = boardService.getTicket(req.params.id);
+    let event = boardService.getEvent(req.params.id);
 
-    res.render('show_ticket', {ticket});
+    let ticket = boardService.getTicket(event, req.params.idt);
+
+    res.render('show_ticket', { event, ticket });
 });
 
+router.get('/event/:id/delete', (req, res) => {
+    
+    let event = boardService.getEvent(req.params.id);
+
+    res.render('delete_event', {event});
+});
+
+router.get('/event/:id/deleted', (req, res) => {
+
+    boardService.deleteEvent(req.params.id);
+
+    res.render('saved_event');
+
+});
+
+router.post('/event/new', (req, res) => {
+
+    let tickets = new Map();
+
+    let { title, description } = req.body;
+
+    boardService.addEvent2(title, description, tickets);
+
+    res.render('saved_event');
+});
 
 export default router;
